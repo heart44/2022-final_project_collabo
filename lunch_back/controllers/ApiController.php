16e7785548220c653a902b94d7dfc64387bbcf82
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-08-02 17:46:43 +0900

    지역 카테고리 수정

---
+++ lunch_back/controllers/ApiController.php
@@ -0,0 +1,50 @@
+<?php
+    namespace application\controllers;
+    use Exception;
+
+    class ApiController extends Controller {
+        //지역 카테고리
+        public function selArea() {
+            return $this->model->selArea();
+        }
+        
+        public function AreaCate1List() {
+            return $this->model->AreaCate1List();
+        }
+    
+        public function AreaCate2List() {
+            $urlPaths = getUrlPaths();
+            if(count($urlPaths) !== 3) {
+                exit();
+            }        
+            $param = [ "cate1" => $urlPaths[2] ];
+            return $this->model->AreaCate2List($param);
+        }
+    
+        public function AreaCate3List() {
+            $urlPaths = getUrlPaths();
+            if(count($urlPaths) !== 4) {
+                exit();
+            }        
+            $param = [ 
+                "cate1" => $urlPaths[2], 
+                "cate2" => $urlPaths[3]
+            ];
+            return $this->model->AreaCate3List($param);
+        }
+
+
+
+
+        //
+        public function selRestaurant() {
+            return $this->model->selRestaurant();
+        }
+
+        public function insBobF() {
+            $json = getJson();
+            print_r($json);
+            return [_RESULT => $this->model->insBobF($json)];
+        }
+        
+    }
